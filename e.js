92df05a6b3e63abdacf54be49d68dcd3c6699fe0
GabrielJandre <167141630+GabrielJandre@users.noreply.github.com>
2024-06-26 20:19:03 -0300

    Add files via upload

--- e.js
+++ e.js
@@ -85,7 +85,128 @@ switch (diaSemana)
 }
 
 //Exercício 11:
-for (let n = 1; n <= 10; n++) 
+let contador = 1;
+while (contador <= 5) 
+{
+    console.log(contador);
+    contador++;
+}
+
+//Exercício 12:
+let numero = 0;
+do  
+{
+    console.log(numero); 
+    numero++;
+}   while (numero < 3);
+
+//Exercício 13:
+let valores = [10, 20, 30, 40, 50];
+let Soma = 0;
+
+for (let i = 0; i < valores.length; i++) 
+{
+    Soma += valores[i];
+}
+
+console.log("A soma dos elementos é:", Soma);
+
+//Ecercício 14:
+let numbers = [6, 18, 9, 12, 16];
+let maximo = numbers[0];
+
+for (let i = 1; i < numbers.length; i++) 
+{
+    if (numbers[i] > maximo) 
     {
-    console.log(n);
+        maximo = numbers[i];
     }
+}
+console.log("O maior Número é:", maximo);
+
+//Exercício 15:
+let number = [5, 23, 32, 29, 9];
+let minimo = number[0];
+
+for (let i = 1; i < number.length; i++) 
+{
+    if (number[i] < minimo) 
+    {
+        minimo = number[i];
+    }
+}
+console.log("O menor Número é", minimo);
+
+//Exercício 16:
+let n = 5;
+let fatorial = 1;
+
+for (let i = 1; i <= n; i++) 
+{
+    fatorial *= i;
+}
+console.log(`O fatorial de ${n} é ${fatorial}`);
+
+//Exercício 17:
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < num.length; i++) 
+{
+    if (num[i] % 2 === 0) 
+    {
+        console.log(`${num[i]} é par`);
+    } 
+    else 
+    {
+        console.log(`${num[i]} é ímpar`);
+    }
+}
+
+//Exercício 18:
+let text = "ola";
+let vogais = ['a', 'e', 'i', 'o', 'u'];
+let contadorr = 0;
+
+for (let i = 0; i < text.length; i++) 
+{
+    let caractere = text[i].toLowerCase();
+    if (vogais.includes(caractere)) 
+    {
+        contadorr++;
+    }
+}
+console.log(`O texto "${text}" possui ${contadorr} vogais.`);
+
+//Exercício 19:
+let texto = "ola";
+let textoInvertido = "";
+
+for (let i = texto.length - 1; i >= 0; i--) 
+{
+    textoInvertido += texto[i];
+}
+
+console.log(`Texto original: ${texto}`);
+console.log(`Texto invertido: ${textoInvertido}`);
+
+//Exercício 20:
+let inicio = 2;
+let fim = 20;
+
+console.log(`Números primos no intervalo de ${inicio} a ${fim}:`);
+for (let num = inicio; num <= fim; num++) 
+{
+    let primo = true;
+    for (let i = 2; i < num; i++) 
+        {
+        if (num % i === 0) 
+        {
+            primo = false;
+            break;
+        }
+    }
+    if (primo && num > 1) 
+    {
+        console.log(num);
+    }
+}
